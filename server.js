ac137f4c55ea69d8f6452581ee6e1d89cdcfdebd
yinzhedy <yinzhe.dy@gmail.com>
2021-11-16 09:33:41 -0500

    deployment via atlas

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Cluster0", 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessTracker", 
 { useNewUrlParser: true,
 useUnifiedTopology: true,
 useCreateIndex: true,
-useFindAndModify: true });
+useFindAndModify: false });
 
 // const db = require("./models");
 
